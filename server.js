e72487526dd0cbe32dc73017a989266d4c6f09f1
Victoria Daug <victoria.daug@gmail.com>
2018-03-05 23:30:07 +0000

    sterver spin

--- server.js
+++ server.js
@@ -1,8 +1,36 @@
 'use strict';
 
-// TEMP: Simple In-Memory Database
-const data = require('./db/notes');
+  const express = require('express');
 
-console.log('hello world!');
+  const data = require('./db/notes');
 
-// INSERT EXPRESS APP CODE HERE...
+  const app = express();
+  app.use(express.static('public'));
+
+  app.get('/api/notes/search', (req, res) => {
+    const searchTerm = req.query.searchTerm;
+    console.log(searchTerm, 'searchTerm')
+    const found = data.filter(item => item.title.includes(searchTerm));
+    console.log(found, 'found')
+    res.status(200).json(found);
+  });
+
+  app.get('/api/notes', (req, res) => {
+    console.log('notes!')
+    res.json(data);
+  });
+
+
+  app.get('/api/notes/:id', (req, res) => {
+    const newID = req.params.id;
+    const rightID = data.find(item => item.id === Number(newID));
+    res.status(200).json(rightID);
+  });
+
+
+  // Listen for incoming connections
+  app.listen(8080, function () {
+    console.info(`Server listening on ${this.address().port}`);
+  }).on('error', err => {
+    console.error(err);
+  });
